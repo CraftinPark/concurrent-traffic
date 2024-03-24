0db5a053a9dc5fbb059506a0417d261de508a7ce
CraftinPark <craftinpark@gmail.com>
2024-03-23 22:45:00 -0700

    first commit, set up car struct, create update function

---
+++ car.h
@@ -0,0 +1,8 @@
+typedef struct {
+    float posx;
+    float posy;
+    float velx;
+    float vely;
+    float accx;
+    float accy;
+} Car;
---
+++ main.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "car.h"
+
+void update_cars(Car *cars, int num_cars, float time) {
+    for (int i = 0; i < num_cars; i++) {
+        cars[i].posx = cars[i].posx + (cars[i].velx * time) + (0.5 * cars[i].accx * time * time); // position equation
+        cars[i].posy = cars[i].posy + (cars[i].vely * time) + (0.5 * cars[i].accy * time * time);
+        cars[i].velx = cars[i].velx + (cars[i].accx * time); // velocity equation
+        cars[i].vely = cars[i].vely + (cars[i].accy * time);
+        printf("car %d: posx: %f, posy: %f ", i, cars[i].posx, cars[i].posy);
+    }
+    printf("\n");
+}
+
+int main() {
+
+    Car *cars; // support up to 100 cars. each car has posx (m), posy (m), velx (m/s), vely (m/s), accx (m/s^2), accy (m/s^2)
+    int num_cars = 2; // number of cars
+    cars = (Car *) malloc(sizeof(Car) * num_cars);
+
+    // initialize problem set
+    // intersection:
+    // - each lane is 3 meters wide. this creates 6mx6m intersection zone.
+    // - cars enter proximity of concurrent traffic 50m before the intersection zone on all four sides.
+    // - this creates 106mx106m plane.
+    // - to simplify, the top left of the intersection zone is (0,0).
+    // - this makes minimum xy (-50,-50) and maximum xy (56,56)
+    cars[0].posx = -50;
+    cars[0].posy = 4.5;
+    cars[0].velx = 15;
+    cars[0].vely = 0;
+    cars[0].accx = 0;
+    cars[0].accy = 0;
+
+    cars[1].posx = 1.5;
+    cars[1].posy = -50;
+    cars[1].velx = 0;
+    cars[1].vely = 15;
+    cars[1].accx = 0;
+    cars[1].accy = 0;
+
+    float simulation_time = 5;  // seconds
+    float update_interval = 0.1; // seconds
+    float time_elapsed = 0;      // seconds
+
+    // print starting position
+    printf("start:\n");
+    for (int i = 0; i < num_cars; i++) {
+        printf("car %d: posx: %f, posy: %f ", i, cars[i].posx, cars[i].posy);
+    }
+    printf("\n");
+
+    while (time_elapsed <= simulation_time) {
+        update_cars(cars, num_cars, update_interval);
+        time_elapsed += update_interval;
+    }
+
+    free(cars);
+    return 0;
+}
