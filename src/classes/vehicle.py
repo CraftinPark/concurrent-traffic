d287cad17bf84cdcfe3d82f3c6c80f11254a987e
CraftinPark <craftinpark@gmail.com>
2024-04-20 18:49:58 -0700

    3 default values for vehicle

--- src/classes/vehicle.py
+++ src/classes/vehicle.py
@@ -1,10 +1,12 @@
+import numpy as np
+
 class Vehicle:
-    position = None     # numpy array[2] describing world coordinates
-    velocity = None     # numpy array[2] describing euclidean velocity vector
-    acceleration = None # numpy array[2] describing euclidean acceleration vector
-    direction = None    # numpy array[2] describing unit directional vector
-    width = None        # float representing width of car in meters. orthogonal to direction
-    length = None       # float representing length of car in meters. parallel to direction
+    position = np.array([0,0])     # numpy array[2] describing world coordinates
+    velocity = np.array([0,0])     # numpy array[2] describing euclidean velocity vector
+    acceleration = np.array([0,0]) # numpy array[2] describing euclidean acceleration vector
+    direction = np.array([1,0])    # numpy array[2] describing unit directional vector
+    width = 1.95                   # float representing width of car in meters. orthogonal to direction
+    length = 4.90                  # float representing length of car in meters. parallel to direction
 
     def __init__(self, position, velocity, acceleration, direction, width, height):
         self.position = position
