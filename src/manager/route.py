87508173163bba435286bc15b55383dd7d9988cc
changoug <tien.chang@mail.utoronto.ca>
2024-06-05 22:02:30 -0400

    added length computation for edges and routes

--- src/manager/route.py
+++ src/manager/route.py
@@ -9,11 +9,11 @@ class Node():
     def __init__(self, position: list):
         self.position = position
 
-    def add_in_edge(self, edge: Edge):
-        self.in_edges.append(edge)
+def add_in_edge(self, node: Node, edge: Edge):
+    node.in_edges.append(edge)
 
-    def add_out_edge(self, edge: Edge):
-        self.out_edges.append(edge)
+def add_out_edge(self, node: Node, edge: Edge):
+    node.out_edges.append(edge)
 
 class Edge():
     start: Node = None
@@ -21,7 +21,7 @@ class Edge():
     curved: bool = True
     center: np.ndarray = None # center is only required for curved edges
 
-    def __init__(self, start: Node, end: Node, curved:bool=False, center=None):
+    def __init__(self, start: Node, end: Node, curved:bool=False, center=None, clockwise: bool=False):
         self.start = start
         self.end = end
         self.curved = curved
@@ -29,10 +29,42 @@ class Edge():
             raise ValueError("center point must be provided if edge is not straight")
         self.center = center
 
+        
+def get_length(edge: Edge) -> float:
+    if not edge.curved:
+        return np.linalg.norm(edge.start.position - edge.end.position)
+
+    radius = np.linalg.norm(edge.start.position-edge.center)
+
+    vector_start = edge.start.position - edge.center.position
+    vector_end = edge.end.position - edge.center.position
+
+    # Dot product of the vectors
+    dot_product = np.dot(vector_start, vector_end)
+
+    # Magnitudes of the vectors
+    magnitude_start = np.linalg.norm(vector_start)
+    magnitude_end = np.linalg.norm(vector_end)
+
+    # Cosine of the angle
+    cos_theta = dot_product / (magnitude_start * magnitude_end)
+
+    # Angle in radians
+    theta = np.arccos(cos_theta)
+
+    if edge.clockwise:
+        theta = 2 * np.pi - theta
+
+    return theta * radius
+
+
 class Route():
     edges: list[Edge] = []
+    length: float
     def __init__(self, edges: list[Edge]):
         self.edges = edges
+        for e in self.edges:
+            length += get_length(e)
 
 def route_position_to_world_position(route: Route, position: float):
     return [0,0]
