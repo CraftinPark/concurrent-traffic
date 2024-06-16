6289f1cfbe46fb13c903838be3011c27347088ed
david <hyt152004@gmail.com>
2024-06-16 11:49:16 -0700

    global iterator created for route id

--- src/main.py
+++ src/main.py
@@ -65,21 +65,21 @@ def main():
     edges.append(Edge(nodes[15], nodes[12], curved=True, center=np.array([-6,-6]), clockwise=False)) # left from west
 
     # routes
-    routes.append(Route(1, [edges[ 1], edges[ 8], edges[ 4]])) # straight from south
-    routes.append(Route(2, [edges[ 1], edges[ 9], edges[ 2]])) # right from south
-    routes.append(Route(3, [edges[ 1], edges[10], edges[ 6]])) # left from south
+    routes.append(Route([edges[ 1], edges[ 8], edges[ 4]])) # straight from south
+    routes.append(Route([edges[ 1], edges[ 9], edges[ 2]])) # right from south
+    routes.append(Route([edges[ 1], edges[10], edges[ 6]])) # left from south
 
-    routes.append(Route(4, [edges[ 3], edges[11], edges[ 6]])) # straight from east
-    routes.append(Route(5, [edges[ 3], edges[12], edges[ 4]])) # right from east
-    routes.append(Route(6, [edges[ 3], edges[13], edges[ 0]])) # left from east
+    routes.append(Route([edges[ 3], edges[11], edges[ 6]])) # straight from east
+    routes.append(Route([edges[ 3], edges[12], edges[ 4]])) # right from east
+    routes.append(Route([edges[ 3], edges[13], edges[ 0]])) # left from east
 
-    routes.append(Route(7, [edges[ 5], edges[14], edges[ 0]])) # straight from north
-    routes.append(Route(8, [edges[ 5], edges[15], edges[ 6]])) # right from north
-    routes.append(Route(9, [edges[ 5], edges[16], edges[ 2]])) # left from north
+    routes.append(Route([edges[ 5], edges[14], edges[ 0]])) # straight from north
+    routes.append(Route([edges[ 5], edges[15], edges[ 6]])) # right from north
+    routes.append(Route([edges[ 5], edges[16], edges[ 2]])) # left from north
 
-    routes.append(Route(10, [edges[ 7], edges[17], edges[ 2]])) # straight from west
-    routes.append(Route(11, [edges[ 7], edges[18], edges[ 0]])) # right from west
-    routes.append(Route(12, [edges[ 7], edges[19], edges[ 4]])) # left from west
+    routes.append(Route([edges[ 7], edges[17], edges[ 2]])) # straight from west
+    routes.append(Route([edges[ 7], edges[18], edges[ 0]])) # right from west
+    routes.append(Route([edges[ 7], edges[19], edges[ 4]])) # left from west
 
     # vehicles.append(Vehicle(0,routes[0],0,8,0,2.23,4.90,1.25,'assets/sedan.png')) # south to north
     # vehicles.append(Vehicle(1,routes[1],0,8,0,2.23,4.90,1.25,'assets/sedan.png')) # south to east
