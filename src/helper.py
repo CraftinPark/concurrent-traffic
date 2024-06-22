4146ecc3c461ae8951447b750fea1a3f7ecaed63
CraftinPark <craftinpark@gmail.com>
2024-06-22 11:07:42 -0700

    cleanup

--- src/helper.py
+++ src/helper.py
@@ -1,12 +1,19 @@
 import numpy as np
-from classes.vehicle import Vehicle
-from manager.command import Command
-from classes.route import Route, route_position_to_world_position, world_position_to_route_position
+from classes.route import Route
 from classes.edge import Edge, CircularEdge
-from classes.node import Node
 import sympy
 from sympy import Point2D
 from itertools import combinations
+
+def get_intersections(routes: list[Route]):
+    intersections = set()
+    for r1, r2 in combinations(routes, 2):
+        for e1 in r1.edges:
+            for e2 in r2.edges:
+                if e1 is e2:
+                    continue
+                intersections.update(get_edge_intersections(r1, r2, e1, e2))
+    return intersections
 
 def get_edge_intersections(route1: Route, route2: Route, edge1: Edge, edge2: Edge) -> set[int, int, tuple[float, float]]: # route_position on r1, route_position on r2
     intersections = set(sympy.intersection(edge1.sympy_obj, edge2.sympy_obj))
@@ -27,38 +34,21 @@ def remove_false_circle_intersects(edge: CircularEdge, intersections: list[Point
     start_angle = np.arctan2(-(edge.start.position[1] - edge.center[1]), edge.start.position[0] - edge.center[0])
     end_angle = np.arctan2(-(edge.end.position[1] - edge.center[1]), edge.end.position[0] - edge.center[0])
 
-    to_remove = set()
+    intersetions_to_remove = set()
 
     for i in intersections:
         i_angle = np.arctan2(-(float(i.y) - edge.center[1]), float(i.x) - edge.center[0])
         if not is_angle_between(start_angle, end_angle, i_angle, edge.clockwise):
-           to_remove.add(i)
+           intersetions_to_remove.add(i)
 
-    intersections.difference_update(to_remove)
+    intersections.difference_update(intersetions_to_remove)
 
 def is_angle_between(start_angle: float, end_angle: float, i_angle: float, clockwise: bool) -> bool:
-
     if start_angle <= end_angle and not clockwise:
         return start_angle <= i_angle <= end_angle
-    
     elif start_angle <= end_angle and clockwise:
         return i_angle <= start_angle or i_angle >= end_angle
-    
     elif start_angle >= end_angle and clockwise:
         return end_angle <= i_angle <= start_angle
-    
     elif start_angle >= end_angle and not clockwise:
         return i_angle <= end_angle or i_angle >= start_angle
-
-def get_intersections(routes: list[Route]):
-
-    intersections = set()
-
-    for r1, r2 in combinations(routes, 2):
-        for e1 in r1.edges:
-            for e2 in r2.edges:
-                if e1 is e2:
-                    continue
-                intersections.update(get_edge_intersections(r1, r2, e1, e2))
-
-    return intersections
