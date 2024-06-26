7e0d7a2f29f0753619482f1e69916c3e30478795
Peter Kim <peterjunek7@gmail.com>
2024-06-25 21:10:00 -0700

    Toggle_update pauses the timer & restart now resets the timer

--- src/simulator/simulator.py
+++ src/simulator/simulator.py
@@ -38,10 +38,12 @@ def run_simulation(initial_vehicles: list[Vehicle], nodes: list[Node], edges: li
         nonlocal clock
         nonlocal delta_time
         nonlocal manager
+        nonlocal time_elapsed
 
         vehicles = vehicle_copy(initial_vehicles)
         clock = pygame.time.Clock()
         delta_time = 0
+        time_elapsed = 0
         manager.reset()
     
     toggle_button = Button((0, 0, 0), (255, 50, 50), (SCREEN_WIDTH/2 - 300, SCREEN_HEIGHT - 100), (300, 50), 'toggle update', toggle_update, ())
@@ -81,10 +83,10 @@ def run_simulation(initial_vehicles: list[Vehicle], nodes: list[Node], edges: li
         if is_run:
             # physical changes to world (updating positions, velocity, etc.)
             update_world(delta_time, vehicles)
+            time_elapsed += delta_time
 
         # updates the screen
         pygame.display.update()
         delta_time = clock.tick(60) / 1000
-        time_elapsed += delta_time
         
     pygame.quit()
