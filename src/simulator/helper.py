de9886f25b341b479f453cf7f54af40c4ecf54cc
CraftinPark <craftinpark@gmail.com>
2024-06-29 11:37:52 -0700

    resizable window and introduction of toolbar

--- src/simulator/helper.py
+++ src/simulator/helper.py
@@ -1,8 +1,21 @@
 import numpy as np
-from .simulator import WORLD_WIDTH, WORLD_HEIGHT, SCREEN_WIDTH, SCREEN_HEIGHT
+from .simulator import WORLD_WIDTH, WORLD_HEIGHT, TOOLBAR_HEIGHT
+from pygame import Surface
 
-def world_to_screen_vector(pos: np.ndarray) -> np.ndarray:
-    return np.array([(pos[0]+(WORLD_WIDTH/2))*SCREEN_WIDTH/WORLD_WIDTH, (pos[1]+(WORLD_HEIGHT/2))*SCREEN_HEIGHT/WORLD_HEIGHT])
+def get_x_y_shift(screen: Surface):
+    x_shift = 0
+    y_shift = 0
+    difference = abs(screen.get_height()-TOOLBAR_HEIGHT - screen.get_width())
+    if screen.get_width() > screen.get_height():
+        x_shift = difference / 2
+    return x_shift, y_shift
 
-def world_to_screen_scalar(x: float) -> float:
-    return x*SCREEN_WIDTH/WORLD_WIDTH
+def world_to_screen_vector(screen: Surface, pos: np.ndarray) -> np.ndarray:
+    render_width = min(screen.get_width(), screen.get_height()-TOOLBAR_HEIGHT)
+    render_height = min(screen.get_width(), screen.get_height()-TOOLBAR_HEIGHT)
+    x_shift, y_shift = get_x_y_shift(screen)
+    return np.array([(pos[0]+(WORLD_WIDTH/2))*render_width/WORLD_WIDTH+x_shift, (pos[1]+(WORLD_HEIGHT/2))*render_height/WORLD_HEIGHT+y_shift])
+
+def world_to_screen_scalar(screen: Surface, x: float) -> float:
+    render_width = min(screen.get_width(), screen.get_height()-TOOLBAR_HEIGHT)
+    return x*render_width/WORLD_WIDTH
--- src/simulator/render.py
+++ src/simulator/render.py
@@ -8,27 +8,28 @@ from classes.route import Route, route_position_to_world_position, direction_at_
 from manager.manager import Manager
 from classes.button import Button
 from .helper import world_to_screen_vector, world_to_screen_scalar
+from .simulator import WORLD_WIDTH, WORLD_HEIGHT, TOOLBAR_HEIGHT
 
 pygame.font.init()
 FONT = pygame.font.SysFont('Consolas', 20)
 
 def render_nodes(screen: Surface, nodes: list[Node]):
     for node in nodes:
-        node_position = world_to_screen_vector(node.position)
+        node_position = world_to_screen_vector(screen, node.position)
         pygame.draw.circle(screen, "red", node_position, 3)
 
 def render_edges(screen: Surface, edges: list[Edge]):
     for edge in edges:
         if isinstance(edge, StraightEdge):
-            start_position = world_to_screen_vector(edge.start.position)
-            end_position   = world_to_screen_vector(edge.end.position)
+            start_position = world_to_screen_vector(screen, edge.start.position)
+            end_position   = world_to_screen_vector(screen, edge.end.position)
             pygame.draw.line(screen, "red", start_position, end_position)
         elif isinstance(edge, CircularEdge):
             # define rect
-            radius = world_to_screen_scalar(np.linalg.norm(edge.start.position-edge.center)) # norm describes distance
+            radius = world_to_screen_scalar(screen, np.linalg.norm(edge.start.position-edge.center)) # norm describes distance
             diameter = radius*2
             arc_rect = pygame.Rect(0,0,diameter,diameter)
-            arc_rect.center = world_to_screen_vector(edge.center)
+            arc_rect.center = world_to_screen_vector(screen, edge.center)
 
             # find angle of start point
             vector_to_start = [edge.start.position[0]-edge.center[0],edge.start.position[1]-edge.center[1]]
@@ -49,16 +50,16 @@ def render_edges(screen: Surface, edges: list[Edge]):
 
 def render_intersections(screen: Surface, intersection_points):
     for intersection in intersection_points:
-        node_position = world_to_screen_vector(np.array(list(intersection[2])))
+        node_position = world_to_screen_vector(screen, np.array(list(intersection[2])))
         pygame.draw.circle(screen, "blue", node_position, 3)
 
 def render_vehicles(screen: Surface, vehicles: list[Vehicle]):
     for vehicle in vehicles:
-        vehicle_screen_width = world_to_screen_scalar(vehicle.width)
-        vehicle_screen_length = world_to_screen_scalar(vehicle.length)
+        vehicle_screen_width = world_to_screen_scalar(screen, vehicle.width)
+        vehicle_screen_length = world_to_screen_scalar(screen, vehicle.length)
 
         vehicle_center_point = route_position_to_world_position(vehicle.route, vehicle.route_position)
-        vehicle_center_screen_pos = world_to_screen_vector(vehicle_center_point)
+        vehicle_center_screen_pos = world_to_screen_vector(screen, vehicle_center_point)
 
         img = pygame.transform.smoothscale(vehicle.image, (vehicle_screen_length, vehicle_screen_width))
         vehicle_angle = direction_at_route_position(vehicle.route, vehicle.route_position)
@@ -70,38 +71,62 @@ def render_vehicles(screen: Surface, vehicles: list[Vehicle]):
 
         pygame.draw.circle(screen, "red", vehicle_center_screen_pos, 3)
 
-def render_buttons(screen: Surface, buttons: list[Button]) -> None:
-    for b in buttons:
-        pygame.draw.rect(screen, b.hover_color if b.is_selected() else b.color, [b.x , b.y, b.width, b.height])
-        if b.text != '':
-            font = pygame.font.SysFont('corbel', 30)
-            text = font.render(b.text, 1, (255, 255, 255))
-            screen.blit(text, (b.x + (b.width/2 - text.get_width()/2), b.y + (b.height/2 - text.get_height()/2)))
+def render_background(screen: Surface):
+    position = world_to_screen_vector(screen, [-WORLD_WIDTH/2,-WORLD_HEIGHT/2])
+    width = world_to_screen_scalar(screen, WORLD_WIDTH)
+    height = world_to_screen_scalar(screen, WORLD_HEIGHT)
+    pygame.draw.rect(screen, "grey", pygame.Rect(position[0], position[1], width, height))
+
+def render_border(screen: Surface):
+    position = world_to_screen_vector(screen, [-WORLD_WIDTH/2,-WORLD_HEIGHT/2])
+    width = world_to_screen_scalar(screen, WORLD_WIDTH)
+    height = world_to_screen_scalar(screen, WORLD_HEIGHT)
+    pygame.draw.rect(screen, "maroon", pygame.Rect(position[0]-3, position[1]-3, width+6, height+6),3)
 
 def render_world(screen: Surface, nodes: list[Node], edges: list[Edge], route_visible: bool, intersection_points):
+    render_background(screen)
     if route_visible:
         render_nodes(screen, nodes)
         render_edges(screen, edges)
     render_intersections(screen, intersection_points)
+    render_border(screen)
     # render_scenery()
 
 def render_manager(screen, manager):
     # draw position
-    manager_screen_pos = world_to_screen_vector(manager.position)
+    manager_screen_pos = world_to_screen_vector(screen, manager.position)
     pygame.draw.circle(screen, "green", manager_screen_pos, 5)
 
     # draw radius circle
-    radius = world_to_screen_scalar(manager.radius)
+    radius = world_to_screen_scalar(screen, manager.radius)
     diameter = radius*2
     arc_rect = pygame.Rect(0,0,diameter,diameter)
-    arc_rect.center = world_to_screen_vector(manager.position)
+    arc_rect.center = world_to_screen_vector(screen, manager.position)
     pygame.draw.arc(screen, "green", arc_rect, 0, 2*np.pi)
 
     for i, vehicle in enumerate(manager.vehicles):
-        text_surface = FONT.render(f"id: {vehicle.id}, pos: {vehicle.route_position:.2f}", False, (0, 0, 0))
+        font = pygame.font.SysFont('Segoe UI', 15)
+        text_surface = font.render(f"id: {vehicle.id}, pos: {vehicle.route_position:.2f}", True, (0, 0, 0))
         screen.blit(text_surface, (5,i*20 + 5))
 
-def render_time(screen, time, width): 
-    # draw time
-    text_surface = FONT.render(f"Time: {time:.3f}", False, (0, 0, 0))
-    screen.blit(text_surface, (width - 170,5))
+def render_time(screen, toolbar_rect, time_elapsed): 
+    font = pygame.font.SysFont('Segoe UI', 15)
+    text_surface = font.render(f"Time: {time_elapsed:.3f}", True, (255, 255, 255))
+    text_rect = text_surface.get_rect()
+    text_rect.right = 150
+    screen.blit(text_surface, text_surface.get_rect(topright = (screen.get_width()-3, screen.get_height()-TOOLBAR_HEIGHT)))
+
+def render_buttons(screen: Surface, buttons: list[Button]) -> None:
+    for b in buttons:
+        b.y = screen.get_height()-TOOLBAR_HEIGHT+50
+        pygame.draw.rect(screen, b.hover_color if b.is_selected() else b.color, [b.x , b.y, b.width, b.height])
+        if b.text != '':
+            font = pygame.font.SysFont('Segoe UI', 15)
+            text = font.render(b.text, 1, (255, 255, 255))
+            screen.blit(text, (b.x + (b.width/2 - text.get_width()/2), b.y + (b.height/2 - text.get_height()/2)))
+
+def render_toolbar(screen, time_elapsed, buttons):
+    toolbar_rect = pygame.Rect(0, screen.get_height()-TOOLBAR_HEIGHT,screen.get_width(),TOOLBAR_HEIGHT)
+    pygame.draw.rect(screen, pygame.Color(80,80,80), toolbar_rect)
+    render_time(screen,toolbar_rect, time_elapsed)
+    render_buttons(screen, buttons)
