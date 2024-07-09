5da91ef6506db4db47596f873a24f4679d107666
david <hyt152004@gmail.com>
2024-07-09 10:20:22 -0700

    implement playback speed

--- src/simulator/update.py
+++ src/simulator/update.py
@@ -1,13 +1,8 @@
 from classes.vehicle import Vehicle
 
-playback_speed_factor = 6
-
 def update_vehicles(delta_time: float, vehicles: list[Vehicle]):
     for vehicle in vehicles:
         vehicle.velocity += vehicle.acceleration * delta_time 
-        vehicle.velocity = 16
-    
-        print(vehicle.velocity)
         vehicle.route_position += vehicle.velocity * delta_time
 
 def update_world(delta_time: float, vehicles: list[Vehicle]):
