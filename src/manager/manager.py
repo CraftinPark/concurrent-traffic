3d03f7a9a13b649abf15981e4faa96f9f249c8bc
changoug <tien.chang@mail.utoronto.ca>
2024-07-23 12:20:48 -0400

    rough broken algo

--- src/manager/manager.py
+++ src/manager/manager.py
@@ -11,11 +11,15 @@ class Collision:
     """A Collision represents a collision between two Vehicles at a given time."""
     vehicle0: Vehicle
     vehicle1: Vehicle
+    delta0: float
+    delta1: float
     time: float
 
-    def __init__(self, vehicle0: Vehicle, vehicle1: Vehicle, time: float) -> None:
+    def __init__(self, vehicle0: Vehicle, vehicle1: Vehicle, delta0: float, delta1: float, time: float) -> None:
         self.vehicle0 = vehicle0
         self.vehicle1 = vehicle1
+        self.delta0 = delta0
+        self.delta1 = delta1
         self.time = time
 
 class Manager:
@@ -31,6 +35,7 @@ class Manager:
         # initialize
         self.position = position
         self.radius = radius
+        self.i = 0
 
 def reset(manager: Manager) -> None:
     """Clear manager.vehicles attribute."""
@@ -39,8 +44,8 @@ def reset(manager: Manager) -> None:
 def manager_event_loop(manager: Manager, vehicles: list[Vehicle], cur_time: float) -> None:
     """Event loop for Manager. Updates manager.vehicles if a Vehicle enters its radius. Also recalculates and sends Commands on update of manager.vehicles."""
     if _update_manager_vehicle_list(manager, vehicles):
-        _compute_and_send_acceleration_commands(manager, cur_time)
         manager.collisions = get_collisions(manager, cur_time)
+        _compute_and_send_acceleration_commands(manager, cur_time)
 
 def _update_manager_vehicle_list(manager: Manager, vehicles: list[Vehicle]) -> bool:
     """Return True if new vehicles have been added to manager.vehicles."""
@@ -61,6 +66,7 @@ def _update_manager_vehicle_list(manager: Manager, vehicles: list[Vehicle]) -> b
         # remove if in list and outside radius
         elif vehicle_in_list and distance_to_vehicle > manager.radius:
             manager.vehicles.remove(vehicle)
+            new_vehicle = True
     return new_vehicle
 
 def get_collisions(manager: Manager, cur_time: float) -> list[Collision]:
@@ -81,7 +87,9 @@ def get_collisions(manager: Manager, cur_time: float) -> list[Collision]:
             # print(f"The objects come within 2.5 meters of each other at t = {time_of_collision}")
             # print(f"{vehicle_pair[0].name}: {route_position_to_world_position(vehicle_pair[0].route, route_position_at_time(vehicle_pair[0], result.x, cur_time))}")
             # print(f"{vehicle_pair[1].name}: {route_position_to_world_position(vehicle_pair[1].route, route_position_at_time(vehicle_pair[1], result.x, cur_time))}")
-            collisions.append(Collision(vehicle_pair[0], vehicle_pair[1], time_of_collision))
+            delta0 = vehicle_pair[0].route.total_length - route_position_at_time(vehicle_pair[0], time_of_collision - cur_time, cur_time)
+            delta1 = vehicle_pair[1].route.total_length - route_position_at_time(vehicle_pair[1], time_of_collision - cur_time, cur_time)
+            collisions.append(Collision(vehicle_pair[0], vehicle_pair[1], delta0, delta1, time_of_collision))
     return collisions
 
 def route_position_at_time(vehicle: Vehicle, delta_time: float, cur_time: float) -> float:
@@ -119,11 +127,25 @@ def time_until_end_of_route(vehicle: Vehicle) -> float:
 
 def _compute_and_send_acceleration_commands(manager: Manager, elapsed_time: float) -> None:
     """Compute and send commands."""
-    for v in manager.vehicles:
-        t, a = _compute_command(elapsed_time)
-        # if v.name == "acc":
-        #     v.command = update_cmd(v.command, t, a, elapsed_time) # this will make cars crash for presets/collision_by_command.json
-        v.command = update_cmd(v.command, t, a, elapsed_time)
+    
+    while manager.collisions != []:
+        for c in manager.collisions:
+            close = c.vehicle0 if c.delta0 <= c.delta1 else c.vehicle1
+            far = c.vehicle0 if c.delta0 > c.delta1 else c.vehicle1
+
+            close_s = 1 if 12 > close.velocity else -1
+            far_s = 1 if 4 > far.velocity else -1
+
+            close_a = close.command(elapsed_time) * 1.2
+            far_a = far.command(elapsed_time) * 0.8
+
+            close_t = np.sqrt(close_s * (12 - close.velocity) / close_a)
+            far_t = np.sqrt(far_s * (4 - far.velocity) / far_a)
+            
+            close.command = update_cmd(close.command, np.array([elapsed_time, elapsed_time + close_t]), np.array([close_a, 0]), elapsed_time)
+            far.command = update_cmd(close.command, np.array([elapsed_time, elapsed_time + far_t]), np.array([far_a, 0]), elapsed_time)
+
+        # manager.collisions = get_collisions(manager, elapsed_time)
 
 def _compute_command(elapsed_time: float) -> tuple[np.array, np.array]:
     """Return np.array of acceleration and time values."""
